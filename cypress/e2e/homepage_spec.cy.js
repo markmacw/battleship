d3d7a8955e5a2eb8d89528360f727d592542013d
Mark MacWhinney <11183326+markmacw@users.noreply.github.com>
2022-08-30 23:43:53 -0400

    additional test for homepage

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -2,4 +2,7 @@ describe('The Home Page', () => {
   it('Successfully loads', () => {
     cy.visit('http://localhost:8000/battleship.html')  
   })
+  it('Title contains "Battleship"',() => {
+    cy.get('h1').contains('Battleship')
+  })
 })
